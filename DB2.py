46b7ca2442538e9e728dfba1acfec282eca1a512
chrisdchoo <139262900+chrisdchoo@users.noreply.github.com>
2023-07-13 23:06:48 -0400

    Update DB2.py

--- DB2.py
+++ DB2.py
@@ -2,4 +2,6 @@ import numpy as np
 import matplotlib as plt
 import pandas as pd
 data = pd.read_csv("DB2_data.csv")
-plt.plot(data.month, data.avg_temp, data.revenue)
+data.plot(x="month", y=["avg_temp", "revenue($)"],
+        kind="line", figsize=(10, 10))
+ 
