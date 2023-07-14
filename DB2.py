cdb9e661a64a37993964c53d17dbc9b08bc7b8bf
chrisdchoo <139262900+chrisdchoo@users.noreply.github.com>
2023-07-13 23:01:07 -0400

    Update DB2.py

--- DB2.py
+++ DB2.py
@@ -1,4 +1,5 @@
 import numpy as np
 import matplotlib as plt
 import pandas as pd
-data = pd.read_csv("DB2_data.xlsx")
+data = pd.read_csv("DB2_data.csv")
+plt.plot(data.month, data.avg_temp, data.revenue)
